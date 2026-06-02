0effc0343f5cd1b05116c7df9cd4e7a3d94e8754
LuizFelipeHilgert <159270455+LuizFelipeHilgert@users.noreply.github.com>
2026-06-02 13:26:31 -0300

    Refactor scheduler and enhance resource management
    Refactor worker and pod definitions, improve resource allocation logic, and add YAML generation for pods.

--- scheduler.py
+++ scheduler.py
@@ -1,596 +1,123 @@
-import random
+import os
 
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
-    "trabalho-worker": {
-        "cpu": 8,
-        "ram": 16,
-        "disk": 100
-    },
-    "trabalho-worker2": {
-        "cpu": 4,
-        "ram": 8,
-        "disk": 200
-    }
-}
-
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
-    {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
-    {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
-]
-
-def choose_worker(pod):
-    best_worker = None
-    best_score = -1
-
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
-            resources["disk"] >= pod["disk"]
-        ):
-
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
-
-            if score > best_score:
-                best_score = score
-                best_worker = name
-
-    return best_worker
-
-for pod in pods:
-
-    worker = choose_worker(pod)
-
-    if worker:
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-import random
-
-workers = {
+WORKERS = {
     "kind-worker": {
         "cpu": 8,
         "ram": 16,
-        "disk": 100
+        "disk": 100,
     },
     "kind-worker2": {
         "cpu": 4,
         "ram": 8,
-        "disk": 200
-    }
+        "disk": 200,
+    },
 }
 
-pods = [
-    {"name": "pod1", "cpu": 1, "ram": 1, "disk": 10},
-    {"name": "pod2", "cpu": 2, "ram": 2, "disk": 20},
-    {"name": "pod3", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod4", "cpu": 3, "ram": 4, "disk": 30},
-    {"name": "pod5", "cpu": 2, "ram": 1, "disk": 15},
-    {"name": "pod6", "cpu": 1, "ram": 2, "disk": 10},
-    {"name": "pod7", "cpu": 2, "ram": 3, "disk": 25},
-    {"name": "pod8", "cpu": 1, "ram": 1, "disk": 5},
-    {"name": "pod9", "cpu": 2, "ram": 2, "disk": 10},
+PODS = [
+    {"name": "pod1",  "cpu": 1, "ram": 1, "disk": 10},
+    {"name": "pod2",  "cpu": 2, "ram": 2, "disk": 20},
+    {"name": "pod3",  "cpu": 1, "ram": 1, "disk": 5},
+    {"name": "pod4",  "cpu": 3, "ram": 4, "disk": 30},
+    {"name": "pod5",  "cpu": 2, "ram": 1, "disk": 15},
+    {"name": "pod6",  "cpu": 1, "ram": 2, "disk": 10},
+    {"name": "pod7",  "cpu": 2, "ram": 3, "disk": 25},
+    {"name": "pod8",  "cpu": 1, "ram": 1, "disk": 5},
+    {"name": "pod9",  "cpu": 2, "ram": 2, "disk": 10},
     {"name": "pod10", "cpu": 1, "ram": 1, "disk": 5},
     {"name": "pod11", "cpu": 2, "ram": 4, "disk": 40},
     {"name": "pod12", "cpu": 1, "ram": 2, "disk": 15},
 ]
 
-def choose_worker(pod):
+# Capacidade inicial de cada worker, usada para normalizar o score.
+INITIAL_CAPACITY = {name: dict(res) for name, res in WORKERS.items()}
 
+# Estado mutável dos workers (recursos disponíveis).
+available = {name: dict(res) for name, res in WORKERS.items()}
+
+
+def choose_worker(pod: dict) -> str | None:
+    """
+    Escolhe o worker com mais recursos disponíveis (most-available).
+
+    O score é calculado como a média dos recursos disponíveis normalizados
+    pela capacidade inicial, evitando que o disco domine por ter valores
+    absolutos maiores.
+
+    Retorna o nome do worker escolhido, ou None se nenhum tiver recursos
+    suficientes para o pod.
+    """
     best_worker = None
-    best_score = -1
+    best_score = -1.0
 
-    for name, resources in workers.items():
-
-        if (
-            resources["cpu"] >= pod["cpu"] and
-            resources["ram"] >= pod["ram"] and
+    for name, resources in available.items():
+        fits = (
+            resources["cpu"]  >= pod["cpu"]  and
+            resources["ram"]  >= pod["ram"]  and
             resources["disk"] >= pod["disk"]
-        ):
+        )
+        if not fits:
+            continue
 
-            score = (
-                resources["cpu"] +
-                resources["ram"] +
-                resources["disk"]
-            )
+        capacity = INITIAL_CAPACITY[name]
+        score = (
+            resources["cpu"]  / capacity["cpu"]  +
+            resources["ram"]  / capacity["ram"]  +
+            resources["disk"] / capacity["disk"]
+        ) / 3
 
-            if score > best_score:
-                best_score = score
-                best_worker = name
+        if score > best_score:
+            best_score = score
+            best_worker = name
 
     return best_worker
 
-for pod in pods:
 
-    worker = choose_worker(pod)
-
-    if worker:
-
-        workers[worker]["cpu"] -= pod["cpu"]
-        workers[worker]["ram"] -= pod["ram"]
-        workers[worker]["disk"] -= pod["disk"]
-
-        print(f"{pod['name']} -> {worker}")
-
-        yaml = f'''
+def generate_yaml(pod_name: str, worker: str) -> str:
+    return f"""\
 apiVersion: v1
 kind: Pod
 metadata:
-  name: {pod['name']}
-
+  name: {pod_name}
 spec:
   nodeSelector:
     kubernetes.io/hostname: {worker}
-
   containers:
   - name: nginx
     image: nginx
-'''
+"""
 
-        with open(f"pods/{pod['name']}.yaml", "w") as f:
-            f.write(yaml)
 
-    else:
-        print(f"{pod['name']} -> SEM RECURSOS")
+def main() -> None:
+    os.makedirs("pods", exist_ok=True)
 
-print("\nRECURSOS FINAIS\n")
+    unscheduled = []
 
-for worker, resources in workers.items():
-    print(worker, resources)
+    for pod in PODS:
+        worker = choose_worker(pod)
+
+        if worker:
+            available[worker]["cpu"]  -= pod["cpu"]
+            available[worker]["ram"]  -= pod["ram"]
+            available[worker]["disk"] -= pod["disk"]
+
+            print(f"{pod['name']} -> {worker}")
+
+            yaml_content = generate_yaml(pod["name"], worker)
+            with open(f"pods/{pod['name']}.yaml", "w") as f:
+                f.write(yaml_content)
+        else:
+            print(f"{pod['name']} -> SEM RECURSOS")
+            unscheduled.append(pod)
+
+    print("\n--- RECURSOS FINAIS ---\n")
+    for name, resources in available.items():
+        print(f"{name}: {resources}")
+
+    if unscheduled:
+        print(f"\n--- PODS NÃO AGENDADOS ({len(unscheduled)}) ---\n")
+        for pod in unscheduled:
+            print(f"  {pod['name']} (cpu={pod['cpu']}, ram={pod['ram']}, disk={pod['disk']})")
+
+
+if __name__ == "__main__":
+    main()
